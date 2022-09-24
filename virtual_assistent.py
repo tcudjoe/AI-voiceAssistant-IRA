1eb047de8995277728564ee979942debc4d8447b
tyra cudjoe <tyra20015@gmail.com>
2022-09-24 11:25:49 +0200

    Committing to put on other pc

--- virtual_assistent.py
+++ virtual_assistent.py
@@ -1,12 +1,4 @@
-# Description: This, IRA, is my OWN virtual assistent program that gets the time, the date, responds with a greeting,
-#              returns information from the internet and much more that we might be making at a later stadium.
-
-
-#pip install pyaudio
-#pip install SpeechRecognition
-#pip install gTTS
-#pip install wikipedia
-
+from urllib import response
 import speech_recognition as sr
 import os
 import pyttsx3
@@ -25,20 +17,13 @@ import requests
 # ignore any warning message
 warnings.filterwarnings("ignore")
 
-hour = datetime.datetime.now().hour
+hour=datetime.datetime.now().hour
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[2].id)
-# for voice in voices:
-#     print(voice, voice.id)
-#     engine.setProperty('voice', voice.id)
-#     engine.say("Hello World!")
-#     engine.runAndWait()
-#     engine.stop()
 
 def speak(text):
-    engine.say(text)
     engine.say(text)
     engine.runAndWait()
 
@@ -56,60 +41,79 @@ def wishMe():
 def takeCommand():
     r=sr.Recognizer()
     with sr.Microphone() as source:
-        # print("Listening...")
+        print("Listening...")
         audio = r.listen(source)
-        said = ""
 
         try:
-            said = r.recognize_google(audio, language='eng-in')
-            print(said)
-        # except Exception as e:
-        #     print("Exception: " + str(e))
-
+            statement = r.recognize_google(audio)
+            print("user said:{statement}\n")
+        except sr.UnknownValueError:
+            print("google speech recognition could not understand")
         except Exception as e:
             speak("Could you repeat that again, please?")
-            return "none"
-        return said.lower()
+            # return "none"
+        return statement
 
-def Wake(text):
-    Wake_Words = ['hey ir-ruh', 'goodmorning ir-ruh', 'good afternoon ir-ruh', 'good evening ir-ruh', 'ir-ruh']
-    text = text.lower()
+def wakeWord(text):
+    WAKE_WORDS = ['hey ira', 'okay ira', 'ira', 'goodmorning ira', 'good afternoon ira', 'good evening ira']
+    text = text.lower()  # Convert the text to all lower case words
+  # Check to see if the users command/text contains a wake word
+    for phrase in WAKE_WORDS:
+        if phrase in text:
+            return True
+  # If the wake word was not found return false
+    return False
 
-    # for phrase in Wake_Words:
-    #     if phrase in text:
-    #         return True
-    # return 0
+def getDate():
+    now = datetime.datetime.now()
+    my_date = datetime.datetime.today
+    weekday = calendar.Calendar.day_name[my_date.weekday()]
+    monthNum = now.month
+    dayNum = now.day
+    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st']
+    return 'Today is ' + weekday + ' ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1] + '.'
 
-# print("Loading IRA")
-# speak("Loading  ir-ruh")
+# print("listening IRA")
+# speak("Loading eira")
+# wishMe()
+# wake_word = "hey ira"
 
 if __name__ == "__main__":
     while True:
-        print('listening')
         text = takeCommand()
+        response = ''
 
-        if text.count(Wake) > 0:
+        if (wakeWord(text) == True):
             wishMe()
-            text = takeCommand()
-
             speak("Tell me, how can i help you?")
 
-            statement = takeCommand().lower()
-            if statement == 0:
+            if text == 0:
                 continue
-            if ["bye", "thank you", "shutdown", "goodbye", "shutdown please", "you can go", "you can shutdown"] in statement:
-                if hour >= 0 and hour < 12:
-                    speak("Goodbye sir, have a wonderful morning!")
-                    print("Goodbye sir, have a wonderful morning!")
-                    print('waiting for your next command...')
-                elif hour >= 12 and hour < 18:
-                    speak("Goodbye sir, have a wonderful afternoon!")
-                    print("Goodbye sir, have a wonderful afternoon!")
-                    print('waiting for your next command...')
-                else:
-                    speak("Goodbye sir, have a wonderful evening!")
-                    print("Goodbye sir, have a wonderful evening!")
-                    print('waiting for your next command...')
+            # if "bye" in text or "thank you" in text or "shutdown" in text or "goodbye" in text or "shutdown please" in text or "you can go" or " you can shutdown" in text:
+            #     if hour >= 0 and hour < 12:
+            #         speak("Goodbye sir, have a wonderful morning!")
+            #         print("Goodbye sir, have a wonderful morning!")
+            #         takeCommand()
+            #     elif hour >= 12 and hour < 18:
+            #         speak("Goodbye sir, have a wonderful afternoon!")
+            #         print("Goodbye sir, have a wonderful afternoon!")
+            #         takeCommand()
 
-        # if (Wake(text) == true):
+            #     else:
+            #         speak("Goodbye sir, have a wonderful evening!")
+            #         print("Goodbye sir, have a wonderful evening!")
+            #         takeCommand()
 
+            if "time" in text:
+                now = datetime.datetime.now
+                if now.hour >= 12:
+                    hour = now.hour
+                    if now.minute < 10 :
+                        minute = '0'+str(now.minute)
+                    else:
+                        minute = str(now.minute)
+
+                speak(' ' + 'it is ' + str(hour) + ':' + minute + ' ' + ' .')
+
+        # speak(response)
